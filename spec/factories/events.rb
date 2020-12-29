ddf425333362ae382bf90d5afab101a2a80d7241
takayuki-s <taka12yuki04@gmail.com>
2020-12-29 13:52:48 +0900

    イベント登録テストコード

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :event do
-    
+    association :user
+    title               {"test"}
+    detail              {"testtext"}
+    event_date          { "2020-12-29" }
+    event_time          { "21:47:00" }
+    category_id         { 1 }
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = FactoryBot.build(:event)
+  end
+
+  context '新規登録がうまくいくとき' do
+    it '全ての項目がバリデーション通り存在すれば登録できる' do
+      expect(@event).to be_valid
+    end
+    it 'detailがなくても登録できる' do
+      @event.detail = ''
+      expect(@event).to be_valid
+    end
+    it 'event_timeがなくても登録できる' do
+      @event.event_time = ''
+      expect(@event).to be_valid
+    end
+  end
+
+  context '新規登録がうまくいかないとき' do
+    it 'titleが空だと登録できない' do
+      @event.title = ''
+      @event.valid?
+      expect(@event.errors.full_messages).to include("Title can't be blank")
+    end
+    it 'event_dateが空だと登録できない' do
+      @event.event_date = ''
+      @event.valid?
+      expect(@event.errors.full_messages).to include("Event date can't be blank")
+    end
+    it 'category_idが空だと登録できない' do
+      @event.category_id = ''
+      @event.valid?
+      expect(@event.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+
 end
